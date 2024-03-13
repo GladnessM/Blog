b9fc69a230f0f772bda440d8b0c37adc18e03993
Gladness M <karicece@gmail.com>
2024-03-13 16:38:44 +0300

    updates

---
+++ welcome.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome To Asterics</title>
+</head>
+<body>
+    <h1>Welcome to Asterics</h1>
+</body>
+</html>
